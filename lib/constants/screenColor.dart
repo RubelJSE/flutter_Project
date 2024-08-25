06fdfd44732f28e3fd9c2861df79f8c585bd2e81
rubel-hossain <rubelhossain5433@gmail.com>
2024-08-26 02:13:32 +0600

    Change Language Using button

--- lib/constants/screenColor.dart
+++ lib/constants/screenColor.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:my_flutter_app/constants/button_class.dart';
-import 'package:my_flutter_app/constants/text_style.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hive/hive.dart';
+import 'package:my_flutter_app/constants/text_style.dart';
 
+import '../main.dart';
+import 'button_class.dart';
 import 'colorTwo.dart';
-
 class ScreenColor extends StatefulWidget {
   const ScreenColor({super.key});
 
@@ -13,100 +14,106 @@ class ScreenColor extends StatefulWidget {
 }
 
 class _ScreenColorState extends State<ScreenColor> {
+  List<bool> isSelected = [true, false];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(AppLocalizations.of(context)?.test??'', style: headingStyle),
-          centerTitle: true,
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Container(
-                  child: Center(
-                      child: Text(
-                    'Color',
-                    style: TextStyle(fontSize: 30),
-                  )),
-                  height: 100,
-                  width: 400,
-                  color: colorOne,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  child: Center(
-                      child: Text(
-                    'Color',
-                    style: TextStyle(fontSize: 30),
-                  )),
-                  height: 100,
-                  width: 400,
-                  color: colorTwo,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  child: Center(
-                      child: Text(
-                    'Color',
-                    style: TextStyle(fontSize: 30),
-                  )),
-                  height: 100,
-                  width: 400,
-                  color: colorThree,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  child: Center(
-                      child: Text(
-                    'Color',
-                    style: TextStyle(fontSize: 30),
-                  )),
-                  height: 100,
-                  width: 400,
-                  color: colorFour,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  child: Center(
-                      child: Text(
-                    'Color',
-                    style: TextStyle(fontSize: 30),
-                  )),
-                  height: 100,
-                  width: 400,
-                  color: colorFive,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                /*Container(
-
-                  child: Center(
-                      child: ButtonClass(title: "Sing Up", onPress: (){
-
-                      },
-                        userName: "Rubel Hossain",
-                      )
-                  ),
-                  height: 50,
-                  width: 400,
-                  color: colorSix,
-                ),*/
-                ButtonClass(title: "SingUp", onPress: (){})
-              ],
-            ),
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)?.test ?? '', style: headingStyle),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              _buildColorContainer(context, AppLocalizations.of(context)?.colorOne ?? '', colorOne),
+              const SizedBox(height: 10),
+              _buildColorContainer(context, 'Color', colorTwo),
+              const SizedBox(height: 10),
+              _buildColorContainer(context, 'Color', colorThree),
+              const SizedBox(height: 10),
+              _buildColorContainer(context, 'Color', colorFour),
+              const SizedBox(height: 10),
+              _buildColorContainer(context, 'Color', colorFive),
+              const SizedBox(height: 10),
+              ButtonClass(title: "Sign Up", onPress: () {}),
+              const SizedBox(height: 10),
+              _languageChange(),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
+
+  Widget _buildColorContainer(BuildContext context, String text, Color color) {
+    return Container(
+      height: 100,
+      width: 400,
+      color: color,
+      child: Center(
+        child: Text(text, style: const TextStyle(fontSize: 30)),
+      ),
+    );
+  }
+
+  Widget _languageChange() {
+    return ToggleButtons(
+      focusColor: Colors.deepOrange,
+      color: Colors.green,
+      highlightColor: Colors.blue,
+      borderColor: Colors.green,
+      fillColor: Colors.green,
+      borderWidth: 1,
+      selectedBorderColor: Colors.red,
+      selectedColor: Colors.blue,
+      borderRadius: BorderRadius.circular(50),
+      constraints: const BoxConstraints(
+        minHeight: 30,
+        maxHeight: 50,
+        minWidth: 40,
+      ),
+      onPressed: (int index) {
+        // Toggle the language between Bengali and English
+        String currentLang = Hive.box('langBox').get('langCode');
+        String newLang = currentLang == 'en' ? 'bn' : 'en';
+
+        // Update the language code in the Hive box
+        Hive.box('langBox').put('langCode', newLang);
+
+        // Print debug information
+        debugPrint('Selected language: $newLang');
+
+        // Change the locale using Application.of(context).setLocale
+        Application.of(context).setLocale(Locale(newLang));
+
+        // Update the selected state of the buttons
+        setState(() {
+          for (int i = 0; i < isSelected.length; i++) {
+            isSelected[i] = (newLang == 'bn' && i == 0) || (newLang == 'en' && i == 1);
+          }
+        });
+      },
+      isSelected: isSelected,
+      children: const <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 2, vertical: 0),
+          child: Text(
+            'bn',
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 2, vertical: 0),
+          child: Text(
+            'en',
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,83 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:my_flutter_app/constants/screenColor.dart';
-import 'package:my_flutter_app/myfirstapp.dart';
-import 'package:my_flutter_app/screen/home_page.dart';
-import 'package:my_flutter_app/screen/home_pagetwo.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hive/hive.dart';
+import 'package:my_flutter_app/constants/button_class.dart';
+import 'package:my_flutter_app/constants/screenColor.dart';
+import 'package:my_flutter_app/constants/text_style.dart';
+import 'package:path_provider/path_provider.dart' as pathProvider;
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var appDocumentDirectory = await pathProvider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  if (!Hive.isBoxOpen('langBox')) {
+    await Hive.openBox('langBox');
+  }
+
+  runApp(const Application());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Application extends StatefulWidget {
+  const Application({super.key});
 
+  @override
+  State<Application> createState() => _ApplicationState();
+
+  // Add this method to access the _ApplicationState
+  static _ApplicationState of(BuildContext context) {
+    return context.findAncestorStateOfType<_ApplicationState>()!;
+  }
+}
+
+class _ApplicationState extends State<Application> {
+  Locale _locale = const Locale('bn');
+
+  @override
+  void initState() {
+    super.initState();
+    initLangLocale();
+  }
+
+  Future<void> initLangLocale() async {
+    if (!Hive.isBoxOpen('langBox')) {
+      await Hive.openBox('langBox');
+    }
+    setState(() {
+      _locale = Locale(Hive.box('langBox').get('langCode', defaultValue: 'en'));
+    });
+  }
+
+  void setLocale(Locale locale) {
+    setState(() {
+      _locale = locale;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    Locale _local = Locale('ar');
-    
-    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      locale: _local,
-      localizationsDelegates: const[
+      locale: _locale,
+      localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
-        
       ],
       supportedLocales: const [
         Locale('bn'),
         Locale('en'),
-        Locale('ar')
+        Locale('ko'),
       ],
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ScreenColor(),
+      home: ScreenColor(),
     );
   }
 }
-
-
 
